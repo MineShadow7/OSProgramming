67ab4f683a0033e3d5bf95ddef58beb947b9f61d
MineShadow7 <shad-karagodin@ya.ru>
2024-05-10 11:51:05 +0300

    Modify build script

--- AssemblyFiles/Linux/gcc_7_4_0/build.sh
+++ AssemblyFiles/Linux/gcc_7_4_0/build.sh
@@ -4,12 +4,15 @@ echo $DIR
 export TARGET=0
 export DEBUG=0
 make -f Makefile
+make -f MakefileExe
 export TARGET=0
 export DEBUG=1
 make -f Makefile
+make -f MakefileExe
 export TARGET=1
 export DEBUG=0
 make -f Makefile
+make -f MakefileExe
 export TARGET=1
 export DEBUG=1
 make -f Makefile
